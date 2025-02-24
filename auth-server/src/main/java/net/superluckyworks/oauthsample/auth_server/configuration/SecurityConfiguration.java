8157b5572b6abd8284de89fe30a2bf2a4dc6b1b2
superlucky <mail.superlucky@gmail.com>
2025-02-24 08:55:47 +0800

    Add login config

--- auth-server/src/main/java/net/superluckyworks/oauthsample/auth_server/configuration/SecurityConfiguration.java
+++ auth-server/src/main/java/net/superluckyworks/oauthsample/auth_server/configuration/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,11 +13,36 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration 
 {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception 
+    {
+        http
+            .cors(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize ->authorize
+                .requestMatchers("/", "/index.html").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(formLogin -> formLogin
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+            );
+
+        return http.build();
+    }  
+
     @Bean
     PasswordEncoder passwordEncoder() 
     {
